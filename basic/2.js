a73824ecbc02c4fd1733917d541793329e9afacd
Harshadvaru <139266660+Harshadvaru@users.noreply.github.com>
2024-02-17 18:14:05 +0000

    commit

---
+++ basic/2.js
@@ -0,0 +1,15 @@
+const accountId = 145523
+let accountEmail = "harshadvaru@gmail.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+
+
+// accountId = 2
+
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "bengaluru"
+
+console.log(accountId);
+
+console.table([accountEmail,accountId,accountPassword,accountCity]);
