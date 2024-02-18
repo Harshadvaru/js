97016c25aca0ce92e41a04119de9fc7638c2d9f9
Harshadvaru <139266660+Harshadvaru@users.noreply.github.com>
2024-02-18 05:46:50 +0000

    Coversion / operation

---
+++ basic/3.js
@@ -0,0 +1,8 @@
+// alert ( 3 + 3) // we are using nodejs,Not browser.
+
+// alert("hello");
+
+// console.log("alert");
+
+window.alert("Hello world!");
+alert("Hello world!");
---
+++ basic/4.js
@@ -0,0 +1,24 @@
+let score = "33abc"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueNumber = Number(score)
+console.log(typeof valueNumber);
+console.log(valueNumber);
+
+
+let isLoggedIn = " "
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 = true o = false
+// "" => false
+// "harshad" = true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber);
