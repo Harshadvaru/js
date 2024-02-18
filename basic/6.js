8c948cae367f33ff5c5f16f32d2adcc4fb7f5633
Harshadvaru <139266660+Harshadvaru@users.noreply.github.com>
2024-02-18 06:03:14 +0000

    Comparison

---
+++ basic/6.js
@@ -0,0 +1,22 @@
+console.log(2 > 1);
+console.log(2 >= 1);
+console.log(2 < 1);
+console.log(2 <= 1);
+console.log(2 == 1);
+console.log(2 != 1);
+
+// deferent datatype compere
+console.log("2" > 1);
+console.log("02" > 1);
+
+// deferent convert in Nan or NUll avoid this type of conversion
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// check properly different datatype 
+console.log("02" === 1);
