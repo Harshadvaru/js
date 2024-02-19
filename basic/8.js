8bae7ea0b807bc7b67d4fd78e15c810bf8b0f55a
Harshadvaru <139266660+Harshadvaru@users.noreply.github.com>
2024-02-19 10:10:53 +0000

    Date

---
+++ basic/8.js
@@ -0,0 +1,17 @@
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toLocaleDateString());
+// console.log(typeof myDate);
+
+// month start in Js is 0.
+// let myCreatedDate = new Date(2023, 7 ,8 , 5 , 3)
+let myCreatedDate = new Date("2023-01-14")
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
