da64c3e6e12770cec7b01cd28ffbbaaf85db2d6e
Harshadvaru <139266660+Harshadvaru@users.noreply.github.com>
2024-02-19 11:12:51 +0000

    Array

--- basic1/Array.js
+++ basic1/Array.js
@@ -1,10 +1,10 @@
 // Array
 
-const myArray = [1,2,3,4,5,6,7,8]
+const myArray = [0,1,2,3,4,5]
 const myHeros = ["Saktiman","Batman"]
 
-console.log(myArray[1]);
-console.log(myHeros);
+// console.log(myArray[1]);
+// console.log(myHeros);
 
 // array methods
 
@@ -12,5 +12,28 @@ console.log(myHeros);
 // myArray.push(9)
 // myArray.pop()
 
-myArray.unshift(9)
-console.log(myArray);
+// myArray.unshift(9)
+// myArray.shift()
+
+// console.log(myArray.includes(9));
+// console.log(myArray.indexOf(3));
+
+// const newArray = myArray.join()
+
+// console.log(myArray);
+// console.log(typeof newArray);
+
+//slice,splice
+
+console.log("A", myArray);
+
+const myn1 =myArray.slice(1,3)
+
+console.log(myn1);
+console.log("B", myArray);
+
+
+const myn2 =myArray.splice(1,3)
+console.log("c",myArray)
+console.log(myn2);
+
