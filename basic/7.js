d4a24792a211b79906e62521442be4ba9d32be41
Harshadvaru <139266660+Harshadvaru@users.noreply.github.com>
2024-02-19 09:34:35 +0000

    Datatype

---
+++ basic/7.js
@@ -0,0 +1,19 @@
+// primitive datatype call by value
+   /* 7 type : string, Number, boolean, null, undefined,Symboll BigInt */
+
+const score = 100;
+const scoreValue = 100.3
+
+const isLoggedIn = false;
+const outsideTemp = null
+let useEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber = 34569875632147896541236548n
+
+// Reference type (Non-Primitie)
+//Array,Objects,Functions,
