8ff82be83e1404ebb9608560267cecc20a1a107e
Harshadvaru <139266660+Harshadvaru@users.noreply.github.com>
2024-02-19 11:02:14 +0000

    Array

---
+++ basic1/Array.js
@@ -0,0 +1,16 @@
+// Array
+
+const myArray = [1,2,3,4,5,6,7,8]
+const myHeros = ["Saktiman","Batman"]
+
+console.log(myArray[1]);
+console.log(myHeros);
+
+// array methods
+
+// myArray.push(6)
+// myArray.push(9)
+// myArray.pop()
+
+myArray.unshift(9)
+console.log(myArray);
