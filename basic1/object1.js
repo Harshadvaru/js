3a78c3cc6a009fc1fa4a910ec82347a5bf3e4f19
Harshadvaru <139266660+Harshadvaru@users.noreply.github.com>
2024-02-20 09:32:33 +0000

    objects

--- basic1/object1.js
+++ basic1/object1.js
@@ -1,7 +1,14 @@
 // singleton
 
 // object literals
-const jUser = {
-    name: "Harshad"
+const jsUser = {
+    name: "Harshad",
+    age: 18,
+    location: "anand",
+    email:"harshadvaru30@gmail.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday","Saturday"]
 }
 
+console.log(jsUser.email)
+console.log(jsUser["email"])
