a1faae2c14f5de99d48c9beb0238215a167d017c
Harshadvaru <139266660+Harshadvaru@users.noreply.github.com>
2024-02-21 09:40:26 +0000

    Comit

--- basic1/object1.js
+++ basic1/object1.js
@@ -7,7 +7,7 @@ const mySym = Symbol("key1")
 const JsUser = {
     name: "Harshad",
     "Full name": "Harshad M. Varu", 
-    mySym : "mykey1",
+    [mySym] : "mykey1",
     age: 18,
     location: "anand",
     email:"harshadvaru30@gmail.com",
@@ -18,4 +18,9 @@ const JsUser = {
 console.log(JsUser.email)
 console.log(JsUser["email"])
 console.log(JsUser["Full name"])
-console.log(typeof JsUser.mySym)
+console.log(JsUser[mySym])
+
+JsUser.email = "harshadahir@gmail.com"
+Object.freeze(JsUser)
+JsUser.email = "harshad@gmail.com"
+console.log(JsUser);
