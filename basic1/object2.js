0bb5547cad4098a781c055152455da98ab14c8a8
Harshadvaru <139266660+Harshadvaru@users.noreply.github.com>
2024-02-22 16:36:19 +0000

    object

--- basic1/object2.js
+++ basic1/object2.js
@@ -56,10 +56,25 @@ const users = [
 ]
 
 users[1].email
-console.log(tinderUser);
+// console.log(tinderUser);
 
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
 
-console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+const course = {
+    coursename: "Ui/Ux design",
+    price: "999",
+    courseInstuctor: "Harshad"
+}
+
+// course.courseInstructor
+
+const {courseInstuctor: instructor} = course
+
+// console.log(courseInstructor);
+console.log(instructor);
+
+
