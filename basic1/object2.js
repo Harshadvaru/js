c3f3fe007c52db4a9c7acf9adb191e0a6fc99cf2
Harshadvaru <139266660+Harshadvaru@users.noreply.github.com>
2024-02-22 16:16:33 +0000

    object

--- basic1/object2.js
+++ basic1/object2.js
@@ -30,5 +30,29 @@ const obj2 = {3: "a", 4: "b"}
 const obj4 = {5: "a", 6: "b"}
 
 // const obj3 = {obj1 ,obj2 }
-const obj3 = Object.assign({},obj1, obj2, obj4)
-console.log(obj3);
+// const obj3 = Object.assign({},obj1, obj2, obj4)
+
+const obj3 = (...obj1, ...obj2)
+
+// console.log(obj3);
+
+const users = [
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    }
+]
+
+users[1].email
