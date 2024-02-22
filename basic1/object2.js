24de8e7c3dc2b04761e55bd9cffb6fe6f122f47f
Harshadvaru <139266660+Harshadvaru@users.noreply.github.com>
2024-02-22 16:26:08 +0000

    object

--- basic1/object2.js
+++ basic1/object2.js
@@ -61,3 +61,5 @@ console.log(tinderUser);
 console.log(Object.keys(tinderUser));
 console.log(Object.values(tinderUser));
 console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
