d86da855a1d603108a38ae5c44d878dae9e84b70
Harshadvaru <139266660+Harshadvaru@users.noreply.github.com>
2024-02-23 15:35:40 +0000

    Function

---
+++ basic1/function1.js
@@ -0,0 +1,13 @@
+// function with function Name
+function sayMyName(){
+    console.log("H");
+    console.log("A");
+    console.log("R");
+    console.log("S");
+    console.log("H");
+    console.log("A");
+    console.log("D");
+}
+// reference of function () execution of function
+
+sayMyName() 
