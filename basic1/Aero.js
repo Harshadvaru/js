79cced19e4b3ab58bd74bd1f54f7abfb394a347f
Harshadvaru <139266660+Harshadvaru@users.noreply.github.com>
2024-02-26 18:10:56 +0000

    Aero

--- basic1/Aero.js
+++ basic1/Aero.js
@@ -48,5 +48,7 @@ const chai = () => {
 // const addTwo= (num1, num2) => (num1 + num2)
 // when ( brect) use for (num1 +num2) ; not use return keyword
 
-const addTwo= (num1, num2) => (username="Harshad")
+// create object in ths js
+const addTwo= (num1, num2) => ({username : "Harshad"})
+
 console.log(addTwo(3,4));
