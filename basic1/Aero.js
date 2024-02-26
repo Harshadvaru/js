e5ba52ef2adf17fa9015cb0e6098de011e0b069b
Harshadvaru <139266660+Harshadvaru@users.noreply.github.com>
2024-02-26 18:08:02 +0000

    Aero

--- basic1/Aero.js
+++ basic1/Aero.js
@@ -43,10 +43,10 @@ const chai = () => {
 
 // const addTwo= (num1, num2) => {
 //     return num1 + num2
-// } when { bracet} use then use return
+// } when { bracet} use then use return keyword 
 
 // const addTwo= (num1, num2) => (num1 + num2)
-// when ( brect) use for (num1 +num2) ;
+// when ( brect) use for (num1 +num2) ; not use return keyword
 
 const addTwo= (num1, num2) => (username="Harshad")
 console.log(addTwo(3,4));
