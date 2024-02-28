228b71bcd74b9179fa8630c1780ee7f14f938817
Harshadvaru <139266660+Harshadvaru@users.noreply.github.com>
2024-02-28 09:44:03 +0000

    IIFE

--- basic1/life.js
+++ basic1/life.js
@@ -1,9 +1,14 @@
 // Immediately Invoked Function Expressions (IIFE)
 
+// named IIFE
 (function chai(){
     console.log(`DB CONNECTED`);
-})()
+})();
 
-( () => {
-    console.log(`DB CONNECTED TWO`);
-} )()
+// Aero function Work First code stop and second code stop
+// Un named IIfe 
+( (name) => {
+    console.log(`DB CONNECTED TWO ${name}`);
+} )('Harshad')
+
+// Do life sepereate using ; and work both IIFE in the code.
