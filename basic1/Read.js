42075ec03fe0e3709d9b51ada2432ebf948793b0
Harshadvaru <139266660+Harshadvaru@users.noreply.github.com>
2024-02-28 09:50:18 +0000

    Read

---
+++ basic1/Read.js
@@ -0,0 +1,8 @@
+/* javascript Execution Context
+Step 1 Global Execution context execute on Head.
+Js is singal threaded 
+
+>three type of execution contex.
+    Global Execution context
+    Function Execution context
+    Eval Execution context
