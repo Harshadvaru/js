8ee48aece7f71e7d37f71cd95c06ca1332ae28f9
Harshadvaru <139266660+Harshadvaru@users.noreply.github.com>
2024-03-03 10:32:30 +0000

    truthy

--- basic1/truthy.js
+++ basic1/truthy.js
@@ -9,4 +9,6 @@ else{
 
 /* falsy values 
 false,0,-0,BigInt 0n,"",null,undefined,NaN
-remaininng value is truthy valu.,
+remaininng value is truthy valu.
+
+string 0 is truthy value,string in false value is true, function(){}, " ",[]
