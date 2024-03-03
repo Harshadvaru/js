fc76f5535dbf26fab53269349f3fbb6125a72b2c
Harshadvaru <139266660+Harshadvaru@users.noreply.github.com>
2024-03-03 10:36:27 +0000

    truthy

--- basic1/truthy.js
+++ basic1/truthy.js
@@ -11,4 +11,14 @@ else{
 false,0,-0,BigInt 0n,"",null,undefined,NaN
 remaininng value is truthy valu.
 
-string 0 is truthy value,string in false value is true, function(){}, " ",[]
+string 0 is truthy value,string in false value is true, function(){}, " ",[] */
+
+if (userEmail.length === 0){
+    console.log("Array is empty");
+}
+
+const emptyObj = {}
+
+if(Object.keys(emptyObj).length === 0){
+    console.log("Object is empty");
+}
