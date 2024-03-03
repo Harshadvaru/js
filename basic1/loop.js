68009ececc0eeedd22541c72bb0a2364f3f2897b
Harshadvaru <139266660+Harshadvaru@users.noreply.github.com>
2024-03-03 12:06:00 +0000

    loop

--- basic1/loop.js
+++ basic1/loop.js
@@ -22,9 +22,11 @@ for (let i = 0; i <= 10; i++) {
 }
 
 let myArray = ["flash","batman","superman"]
-console.log(myArray.length);
+// console.log(myArray.length);
 for (let index = 0; index < myArray.length; index++) {
     const element = myArray[index];
-    console.log(element);
+    // console.log(element);
     
 }
+
+// 
