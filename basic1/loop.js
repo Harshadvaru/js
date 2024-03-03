2e673118f90675963fdc524981c4a46e47599b6a
Harshadvaru <139266660+Harshadvaru@users.noreply.github.com>
2024-03-03 11:45:44 +0000

    loop

---
+++ basic1/loop.js
@@ -0,0 +1,13 @@
+// for loop
+//                  check the length      increase length
+
+for (let i = 0; i <= 10; i++) {
+    const element = i;
+    if (element == 5){
+        console.log("5 is the best number");
+    }
+    console.log(element);
+    
+}
+// not declare not accesseble 
+// console.log(element);
