8ea78948eb7058b3abdc9cac901629d2d2dbf5bc
Harshadvaru <139266660+Harshadvaru@users.noreply.github.com>
2024-03-03 12:13:05 +0000

    loop

--- basic1/loop.js
+++ basic1/loop.js
@@ -42,9 +42,17 @@ for (let index = 1; index <=20; index++) {
 
 for (let index = 1; index <=20; index++) {
     if(index == 5){
-        console.log(`Detected 5`);
-        continue
+        // console.log(`Detected 5`);
+        continue;
     }
-    console.log(`value of i is ${index}`);
-    
+    // console.log(`value of i is ${index}`);
 }
+
+
+
+// while loop 
+let index =0 ;
+ while (index <= 10) {
+        console.log(`Value of index is ${index}`);
+        index = index + 2
+ }
