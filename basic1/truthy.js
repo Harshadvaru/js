2cbea1df8b4ea881867a69f2f40ba88d3563ff6b
Harshadvaru <139266660+Harshadvaru@users.noreply.github.com>
2024-03-03 10:46:53 +0000

    truthy

--- basic1/truthy.js
+++ basic1/truthy.js
@@ -23,12 +23,19 @@ if(Object.keys(emptyObj).length === 0){
     console.log("Object is empty");
 }
 
-// Nullish Coalescing operator (??): null underfined
+// Nullish Coalescing operator (??): null underfined check the null value safety only use null / underfined use the first valueto operate
 
 let val1;
-val1 = 5 ?? 10
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+// val1 = undefined ?? 15
+val1 = null ?? 10 ?? 100
 
+// console.log(val1);
 
+// Terniary Operator
 
+// condition ? true : false
 
-console.log(val1);
+const iceTeaPrice = 100
+iceTeaPrice <= 80 ? console.log("less than 80") : console.log("more than 80")
