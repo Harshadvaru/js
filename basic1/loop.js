e6ee9f5e617c33d8a56bce5b525e26e1553d7115
Harshadvaru <139266660+Harshadvaru@users.noreply.github.com>
2024-03-03 12:01:12 +0000

    loop

--- basic1/loop.js
+++ basic1/loop.js
@@ -4,10 +4,20 @@
 for (let i = 0; i <= 10; i++) {
     const element = i;
     if (element == 5){
-        console.log("5 is the best number");
+        //console.log("5 is the best number");
     }
-    console.log(element);
+    //console.log(element);
     
 }
 // not declare not accesseble 
 // console.log(element);
+
+// create table of 0 to 10 
+for (let i = 0; i <= 10; i++) {
+    console.log(`Outer loop value: ${i}`);
+    for (let j = 0; j <= 10; j++){
+        // console.log(`Inner loop value ${j} and inner loop ${i}`);
+        console.log(i + '*' + j + ' = ' + i*j );
+    }
+    
+}
