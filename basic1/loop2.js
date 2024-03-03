f6ad168eed94b95caecfc851274ea6f41b104e90
Harshadvaru <139266660+Harshadvaru@users.noreply.github.com>
2024-03-03 16:14:38 +0000

    loop

--- basic1/loop2.js
+++ basic1/loop2.js
@@ -10,3 +10,43 @@ for (const key in map) {
 }
 
 // object for in loop or array ke upar for off loop 
+
+const coding =  ["js","ruby","java","python","cpp"]
+
+// callback function
+// coding.forEach( function (val) {
+//     console.log(val);
+// })
+
+// coding.forEach( () => {
+//     console.log(item);
+// })
+
+// function printMe(item){
+//     console.log(item);
+// }
+
+// coding.forEach(printMe)
+
+// coding.forEach( (item, index, arr)=> {
+//     console.log(item, index, arr);
+// })
+
+const myCoding = [
+    {
+        languageName: "javascript",
+        languageFileName: "js"
+    },
+    {
+        languageName: "java",
+        languageFileName: "java"
+    },
+    {
+        languageName: "python",
+        languageFileName: "py"
+    },
+]
+// array ke andar object check kar
+myCoding.forEach((item) => {
+    console.log(item.languageName);
+})
