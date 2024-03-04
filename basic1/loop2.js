c5374e7447026e60f11a7763fc07d663cabe4a8f
Harshadvaru <139266660+Harshadvaru@users.noreply.github.com>
2024-03-04 05:09:45 +0000

    loop

--- basic1/loop2.js
+++ basic1/loop2.js
@@ -121,9 +121,12 @@ const books = [
 
  const myNums =  [1,2,3]
 
- const myTotal = myNums.reduce(function (acc,curval){
-    console.log(`acc: ${acc} and currval: ${curval}`);
-    return acc + curval
- }, 0)
+//  const myTotal = myNums.reduce(function (acc,curval){
+//     console.log(`acc: ${acc} and currval: ${curval}`);
+//     return acc + curval
+//  }, 0)
+
+// when we not open scope than not use the return
+const myTotal = myNums.reduce((acc, curr) => acc+curr, 0)
 
  console.log(myTotal);
