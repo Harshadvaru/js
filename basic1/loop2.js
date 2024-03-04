a2dae157824f08b15c3957a1428adcdad0a674fb
Harshadvaru <139266660+Harshadvaru@users.noreply.github.com>
2024-03-04 04:35:26 +0000

    loop

--- basic1/loop2.js
+++ basic1/loop2.js
@@ -59,7 +59,7 @@ myCoding.forEach((item) => {
 //  })
 
 //  console.log(values);
-
+// 1
 const myNums = [1,2,3,4,5,6,7,8,9,10]
 
 // filter ke andar call back function milega
@@ -67,14 +67,34 @@ const myNums = [1,2,3,4,5,6,7,8,9,10]
 // const newNums = myNums.filter((num) => {
 //     return num > 4
 // })
+// or both can be use
 
+// 2
 // declare array
-const newNums = []
-// use forEach loop
-myNums.forEach( (num) => {
-    if(num > 4){
-        newNums.push(num)
-    }
-})
-// print the newNums
-console.log(newNums);
+// const newNums = []
+// // use forEach loop
+// myNums.forEach( (num) => {
+//     if(num > 4){
+//         newNums.push(num)
+//     }
+// })
+// // print the newNums
+// console.log(newNums);
+
+const books = [
+    // there are multiple object
+    {title: 'Book One', genre: 'Fiction', publish:1981,edition:2004},
+    {title: 'Book Two', genre: 'Non-Fiction', publish:1982,edition:2005},
+    {title: 'Book Three', genre: 'History', publish:1983,edition:2006},
+    {title: 'Book Four', genre: 'Fiction', publish:1984,edition:2007},
+    {title: 'Book Five', genre: 'Non-Fiction', publish:1985,edition:2008},
+    {title: 'Book Six', genre: 'History', publish:1986,edition:2009},
+    {title: 'Book Seven', genre: 'Fiction', publish:1987,edition:2010},
+    {title: 'Book Eight', genre: 'Non-Fiction', publish:1988,edition:2011}
+];
+
+let userBooks = books.filter( (bk) => bk.genre === 'History')
+// there overwrite the function in this variable
+ userBooks = books.filter( (bk) => { return bk.publish <= 2000 })
+
+console.log(userBooks);
