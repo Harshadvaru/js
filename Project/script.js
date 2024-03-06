20e05df26eb4af62aedda9c8125b8cad60926f2d
Harshadvaru <139266660+Harshadvaru@users.noreply.github.com>
2024-03-06 09:03:43 +0000

    Project

--- Project/script.js
+++ Project/script.js
@@ -6,6 +6,7 @@ button.forEach(function(button){
     console.log(button);
     // add event lisner  in this line add the event listner and perform the event 
     button.addEventListener('click',function(event){
-
+        console.log(event)
+        console.log(event.target)
     })
 });
