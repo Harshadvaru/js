9ac215e77c7ff0dd4c5aa993b34c5eafff12c410
Harshadvaru <139266660+Harshadvaru@users.noreply.github.com>
2024-03-06 09:00:40 +0000

    Project

--- Project/script.js
+++ Project/script.js
@@ -1,3 +1,7 @@
 const button = document.querySelectorAll('.button')
 const body = document.querySelector("body")
 // console.log(button);
+
+button.forEach(function(button){
+    console.log(button);
+})
