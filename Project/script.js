3340a2ad3a2d6557638c323d955ac2cf4b4b5eae
Harshadvaru <139266660+Harshadvaru@users.noreply.github.com>
2024-03-06 09:12:13 +0000

    Project

--- Project/script.js
+++ Project/script.js
@@ -11,5 +11,17 @@ button.forEach(function(button){
         if(event.target.id === 'grey'){
             body.style.backgroundColor = event.target.id;
         }
-    })
+        if(event.target.id === 'white'){
+            body.style.backgroundColor = event.target.id;
+        }
+        if(event.target.id === 'blue'){
+            body.style.backgroundColor = event.target.id;
+        }
+        if(event.target.id === 'yellow'){
+            body.style.backgroundColor = event.target.id;
+        }
+        if(event.target.id === 'purple'){
+            body.style.backgroundColor = event.target.id;
+        }
+    });
 });
