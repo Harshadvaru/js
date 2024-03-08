60e283663318633144e63c170577e01207552ce1
Harshadvaru <139266660+Harshadvaru@users.noreply.github.com>
2024-03-08 13:49:28 +0000

    commit

--- project 3/script.js
+++ project 3/script.js
@@ -3,5 +3,12 @@
 // or 
 // const clock document.querySelector('#clock')
 
-let date = new Date()
-console.log(date.toLocaleTimeString());
+
+
+
+// thet method use for take time after run many time
+setInterval(function(){
+    let date = new Date()
+    // console.log(date.toLocaleTimeString());
+    clock.innerHTML = date.toLocaleTimeString();
+}, 1000)
