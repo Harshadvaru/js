00a5d145d26f4804a103a2ee8b79c873c9b439c1
Harshadvaru <139266660+Harshadvaru@users.noreply.github.com>
2024-03-09 12:40:17 +0000

    script

--- Project 4/script.js
+++ Project 4/script.js
@@ -14,18 +14,59 @@ let numGuess = 1;
 
 let playGame = true;
 
+if(playGame){
+    submit.addEventListener('click',function(e){
+        e.preventDefault();
+        const guess = parseInt(userInput.value);
+        console.log(guess);
+        validateGuess(guess);
+    })
+}
 function validateGuess(guess){
+    if(isNaN(guess)){
+        alert('Please enter a valid number')
+    } else if(guess < 1){
+        alert('Please enter a valid number more than 1')
+    } else if(guess > 100){
+        alert('Please enter a valid number less than 100')
+    } else{
+        prevGuess.push(guess)
+        if(numGuess === 11){
+            displayGuess(guess)
+            displayMessage(`game Over.Random number was ${randomNumber}`)
+            endGame()
+        }else{
+            displayGuess(guess)
+            checkGuess(guess)
+        }
+    }
     // method use for guess for validation
 }
 
 function checkGuess(guess){
-    // 
+    if(guess === randomNumber){
+        displayMessage(`You guessed it right`)
+        endGame()
+    } else if(guess < randomNumber){
+        displayMessage(`Number is TOO  low!`)
+    } else if(guess > randomNumber){
+        displayMessage(`Number is TOO  HIGH!`)
+    }
 }
 
 function displayGuess(guess){
-    
+    //
 }
 
 function displayMessage(message){
     // return the message
 }
+
+function endGame(){
+    // 
+}
+
+function newGame(){
+    //
+}
+
