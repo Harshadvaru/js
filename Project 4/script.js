406b89654c805cc76bf4aca0301dc2867913c254
Harshadvaru <139266660+Harshadvaru@users.noreply.github.com>
2024-03-09 11:39:04 +0000

    script

--- Project 4/script.js
+++ Project 4/script.js
@@ -4,4 +4,6 @@ const submit = document.querySelector('#subt');
 const userInput = document.querySelector('#guessField');
 const guessSlot = document.querySelector('.guesses');
 const remaining = document.querySelector('.lastResult');
+const lowOrHi = document.querySelector('.lowOrHi');
+const startOver = document.querySelector('.resultParas');
 
