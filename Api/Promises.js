1c47cd0c3152668b3540586c27681787ec80a3e5
Harshadvaru <139266660+Harshadvaru@users.noreply.github.com>
2024-04-07 05:33:16 +0000

    Promises

--- Api/Promises.js
+++ Api/Promises.js
@@ -1,8 +1,9 @@
-const promiseOne = new Promise(function(resolved, reject){
+const promiseOne = new Promise(function(resolve, reject){
     // do an async task
     // do calls, cryptography, network
     setTimeout(function(){
         console.log('Async task is complete');
+        resolve()
     }, 1000)
 })  // create new instance using new keyword
 
@@ -10,4 +11,12 @@ const promiseOne = new Promise(function(resolved, reject){
 
 promiseOne.then(function(){
     console.log("Promise consumed");
+})
+
+new Promise(function(resolve, reject){
+    setTimeout(function(){
+        console.log("Async task 2");
+        resolve()
+    }, 1000)
+
 })
